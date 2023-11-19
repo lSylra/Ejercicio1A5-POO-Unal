0072fd738fef0308ab692c7f4f4b341cc4a26d62
lSylra <143262189+lSylra@users.noreply.github.com>
2023-11-18 20:53:48 -0500

    Add files via upload

---
+++ Ejercicio1A5.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio1a5;
+
+/**
+ *
+ * @author USER
+ */
+public class Ejercicio1A5 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true);
+    }
+}
